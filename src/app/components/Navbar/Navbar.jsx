2d230c8bff1281682909452510d6ebe68fe22647
Felipe Pimentel de Aguiar <felipe.aguiar@philips.com>
2018-01-21 12:59:51 -0200

    fix: change navbar theme

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-	<nav>
+	<nav className="teal lighten-1">
 		<div className="nav-wrapper">
-			<a href="#" className="brand-logo">
+			<div href="#" className="brand-logo">
 				<Link to="/">VH-Jobs</Link>
-			</a>
+			</div>
 			<ul id="nav-mobile" className="right hide-on-med-and-down">
 				<li><Link to="/jobs">Jobs</Link></li>
 				<li><Link to="/about">About</Link></li>
