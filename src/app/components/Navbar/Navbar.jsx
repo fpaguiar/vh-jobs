b9e9f73ee921d25fbba25758ff051e31553811e2
Felipe Pimentel de Aguiar <felipe.aguiar@philips.com>
2018-01-21 20:52:23 -0200

    refactor: navbar uses Logo component

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './Navbar.scss';
+import Logo from 'Components/Logo/Logo';
 
 const Navbar = () => (
 	<nav className="teal orange accent-3">
 		<div className="nav-wrapper">
-			<div href="#" className="logo brand-logo">
-				<Link to="/">GetMeHired</Link>
+			<div href="#" className="brand-logo">
+				<Logo />
 			</div>
 			<ul id="nav-mobile" className="right hide-on-med-and-down">
 				<li><Link to="/jobs">Jobs</Link></li>
